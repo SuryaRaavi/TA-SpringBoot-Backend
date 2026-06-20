d5752aca5095257a141c54a9133cbffb16e0738b
SuryaRaavi <sryraavi.adiputra@gmail.com>
2026-06-20 12:59:54 +0700

    docs: Update measurement example 2

--- src/main/java/com/ta/managementproject/example/NestingExample.java
+++ src/main/java/com/ta/managementproject/example/NestingExample.java
@@ -2,16 +2,14 @@ package com.ta.managementproject.example;
 
 import java.util.List;
 
-// Total CYC: 5, COG: 11
+// Total CYC: 4, COG: 8
 public class NestingExample {
     public int countOverdueTasks(List<Task> tasks) { // CYC: 1
         int count = 0;
         for (Task task : tasks) { // CYC: 1, COG: 1
-            if (task.isOverdue()) { // CYC: 1, COG: 2 (+1 pinalti dari level nesting 1)
-                if (task.getPriority() == 3) { // CYC: 1, COG: 3 (+2 pinalti dari level nesting 2)
+            if (task.isOverdue()) { // CYC: 1, COG: 2 (+1 pinalti untuk blok if else karena level nesting 1)
+                if (task.getPriority() == 3) { // CYC: 1, COG: 3 (+2 pinalti untuk blok if else karena level nesting 2)
                     count += 2;
-                } else if (task.getPriority() == 4){ // CYC: 1, COG: 3 (+2 pinalti dari level nesting 2)
-                    count += 3;
                 } else { // COG: 1
                     count += 1;
                 }
